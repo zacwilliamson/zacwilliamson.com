db766628ec1ea3fd9cbac7bdf294103ffeb0680e
supersakana <zacwilliamson@icloud.com>
2024-11-20 17:43:57 -0600

    updated homepage images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -273,13 +273,12 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            Software designer, founder, and amateur astronaut.
+            Software developer, traveler, and writter.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
-            own terms.
+            I’m Zac, a full-stack software developer from Virginia Beach.
+            I’ve worked on web projects in various industries such as Waste Managment,
+            Real Estate, and Healthcare. Love to learn new tech and collaborate with all types of people.
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink href="#" aria-label="Follow on X" icon={XIcon} />
